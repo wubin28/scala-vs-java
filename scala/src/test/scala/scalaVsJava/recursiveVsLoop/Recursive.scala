7d8f62d91889f6e92df150a6ed14ace32745bf8b
Ben Wu <wubin28@gmail.com>
2015-05-16 23:49:09 +0800

    Reformated.

--- scala/src/test/scala/scalaVsJava/recursiveVsLoop/Recursive.scala
+++ scala/src/test/scala/scalaVsJava/recursiveVsLoop/Recursive.scala
@@ -10,7 +10,8 @@ class Recursive extends FunSuite{
   def firstScalaFileIndex(strings: Array[String]) = {
     def searchFrom(i: Int): Int = {
       if (i >= strings.length) -1
-      else if (strings(i).startsWith("-")) searchFrom(i + 1)
+      else if (strings(i).startsWith("-"))
+        searchFrom(i + 1)
       else if (strings(i).endsWith(".scala")) i
       else searchFrom(i + 1)
     }
@@ -18,6 +19,7 @@ class Recursive extends FunSuite{
   }
 
   test("in Scala recursive is over loop") {
-    assert(1 === firstScalaFileIndex(Array("-xvz", "helloWorld.scala")))
+    assert(1 === firstScalaFileIndex(
+      Array("-xvz", "helloWorld.scala")))
   }
 }
