147ac9b1e8a629c2122bff03ef353d30e8c24bd8
Ben Wu <wubin28@gmail.com>
2015-05-16 21:39:34 +0800

    Refactored the java test 'in_java_loop_is_more_popular_than_recursive' to use recursive.

--- java/src/test/java/scalaVsJava/recursiveVsLoop/Loop.java
+++ java/src/test/java/scalaVsJava/recursiveVsLoop/Loop.java
@@ -9,21 +9,27 @@ import static org.junit.Assert.assertEquals;
  */
 public class Loop {
     private class FileNameLocator {
-        public int firstScalaFileIndex(String[] strings) {
-            String string;
-            for (int i = 0; i < strings.length; i++) {
-                string = strings[i];
-                if (string.startsWith("-")) continue;
-                if (string.endsWith(".scala")) return i;
-            }
-            return -1;
+        private final String[] strings;
+
+        public FileNameLocator(String[] strings) {
+            this.strings = strings;
+        }
+
+        public int firstScalaFileIndex() {
+            return searchFrom(0);
+        }
+
+        private int searchFrom(int index) {
+            if (index >= strings.length) return -1;
+            if (strings[index].startsWith("-")) searchFrom(index + 1);
+            if (strings[index].endsWith(".scala")) return index;
+            return searchFrom(index + 1);
         }
     }
 
     @Test
     public void in_java_loop_is_more_popular_than_recursive() {
-        FileNameLocator fileNameLocator = new FileNameLocator();
-        assertEquals(1, fileNameLocator.firstScalaFileIndex(
-                new String[]{"-xvz", "helloWorld.scala"}));
+        FileNameLocator fileNameLocator = new FileNameLocator(new String[]{"-xvz", "helloWorld.scala"});
+        assertEquals(1, fileNameLocator.firstScalaFileIndex());
     }
 }
